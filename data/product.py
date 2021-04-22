32d0489bd7e0a0eb1ecfe4ca169d2fe99a9daf7a
xivol <loshkarev.i@gmail.com>
2021-04-22 19:56:03 +0300

    products merged

--- data/product.py
+++ data/product.py
@@ -9,14 +9,17 @@ class Product(SqlAlchemyBase):
     __tablename__ = 'products'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     photo_1 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     photo_2 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     photo_3 = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     descript = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.Float, nullable=False)
+
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+
     categories = orm.relation("ProductCategories", back_populates="product")
 
 
